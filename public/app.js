483850b49df44d79799826f53cf7af6275a539f5
Ahsan Jawed <ahsan.ahmed99@ymail.com>
2017-02-28 22:10:31 +0500

    First JavaScript and First GitHub

---
+++ public/app.js
@@ -0,0 +1,13 @@
+alert("Hello Visitor");
+function add(){
+    alert(Number(document.getElementById("num1").value)+Number(document.getElementById("num2").value));    
+}
+function sub(){ 
+    alert(Number(document.getElementById("num1").value)-Number(document.getElementById("num2").value));  
+}
+function div(){
+    alert(Number(document.getElementById("num1").value)/Number(document.getElementById("num2").value));
+}
+function mul(){
+    alert(Number(document.getElementById("num1").value)*Number(document.getElementById("num2").value));    
+}
